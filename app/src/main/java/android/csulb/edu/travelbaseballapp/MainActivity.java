0817326b0bc34eee5c67f8752a682473c30774f4
xKVSx <jabroni_247@hotmail.com>
2018-09-27 15:10:52 -0700

    All emails are now added to Google Calendar API. Next task is to create the Widget displaying the next upcoming event.

--- app/src/main/java/android/csulb/edu/travelbaseballapp/MainActivity.java
+++ app/src/main/java/android/csulb/edu/travelbaseballapp/MainActivity.java
@@ -1,27 +1,49 @@
 package android.csulb.edu.travelbaseballapp;
 
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.csulb.edu.travelbaseballapp.pojos.BaseballEvent;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.firebase.ui.auth.AuthUI;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Date;
+
+import static android.Manifest.permission.GET_ACCOUNTS;
 
 public class MainActivity extends AppCompatActivity {
-    // Choose an arbitrary request code value
-    private static final int RC_SIGN_IN = 316;
+
+    private static final int RC_SIGN_IN = 316;            // Choose an arbitrary request code value
+    public static final int MY_PERMISSIONS_REQUEST_GET_ACCOUNTS = 123;
+    public static final int REQUEST_GOOGLE_PLAY_SERVICES = 1002;
+    public static final String GOOGLE_CREDENTIAL_EMAIL = "credential_email";
     private FirebaseAuth mFirebaseAuth;
     private FirebaseAuth.AuthStateListener mAuthStateListener;
+    private FirebaseUser mUser;
     private Toolbar mToolbar;
+    private FirebaseDatabase mFirebaseDatabase;
+    private DatabaseReference mUsersDatabaseReference;
+    private DatabaseReference mEventsDatabaseReference;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,10 +52,69 @@ public class MainActivity extends AppCompatActivity {
         mToolbar = findViewById(R.id.main_toolbar);
         setSupportActionBar(mToolbar);
 
-        //initializes Firebase components
+        //Initialize FirebaseDatabase
+        mFirebaseDatabase = FirebaseDatabase.getInstance();
+        mUsersDatabaseReference = mFirebaseDatabase.getReference().child("users");
+        mEventsDatabaseReference = mFirebaseDatabase.getReference().child("events");
+
+        //initializes FirebaseAuth components
         initializeFirebaseAuth();
         initializeFirebaseAuthListener();
 
+        loadMostRecentEvent();
+    }
+
+    private void loadMostRecentEvent(){
+        mEventsDatabaseReference.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                TextView summaryView = findViewById(R.id.eventSummary);
+                TextView locationView = findViewById(R.id.eventLocation);
+                TextView descriptionView = findViewById(R.id.eventDescription);
+                TextView startTimeView = findViewById(R.id.eventStartTime);
+                ArrayList<BaseballEvent> baseballEventList = new ArrayList<>();
+                Date currentDate = new Date();
+                Date minDate = null;
+                int minIndex = 0;
+
+                for(DataSnapshot outerPostSnapshot: dataSnapshot.getChildren()) {
+                    BaseballEvent baseballEvent = outerPostSnapshot.getValue(BaseballEvent.class);
+                    baseballEventList.add(baseballEvent);
+                }
+
+                //find the most current event
+                if(baseballEventList.size() != 0) {
+                    minDate = baseballEventList.get(0).getStartDate();
+
+                    for (int i = 1; i < baseballEventList.size(); i++) {
+                        Date nextDate = baseballEventList.get(i).getStartDate();
+
+                        if (minDate.after(nextDate) && minDate.before(currentDate)) {
+                            minDate = nextDate;
+                            minIndex = i;
+                        }
+                    }
+                }
+                else
+                    summaryView.setText(getString(R.string.no_events));
+
+                 if(minDate.before(currentDate)) {
+                     summaryView.setText(getString(R.string.no_events));
+                 }
+                 else {
+                     //set the minDate to the View
+                     summaryView.setText(baseballEventList.get(minIndex).getSummary());
+                     descriptionView.setText(baseballEventList.get(minIndex).getDescription());
+                     locationView.setText(baseballEventList.get(minIndex).getLocation());
+                     startTimeView.setText(baseballEventList.get(minIndex).getDisplayTimeStart());
+                 }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
     }
 
     @Override
@@ -63,6 +144,7 @@ public class MainActivity extends AppCompatActivity {
                 return true;
             case R.id.schedule:
                 Intent scheduleIntent = new Intent(this, ScheduleActivity.class);
+                scheduleIntent.putExtra(GOOGLE_CREDENTIAL_EMAIL, mUser.getEmail());
                 startActivity(scheduleIntent);
                 return true;
             default:
@@ -78,11 +160,29 @@ public class MainActivity extends AppCompatActivity {
         mAuthStateListener = new FirebaseAuth.AuthStateListener() {
             @Override
             public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
-                FirebaseUser user = firebaseAuth.getCurrentUser();
+                final FirebaseUser user = firebaseAuth.getCurrentUser();
 
                 if(user != null){
                     //user is signed in
-                    Toast.makeText(MainActivity.this, "You are signed into the Baseball Travel App", Toast.LENGTH_LONG).show();
+                    mUser = user;
+                    //request user permission to use access accounts. Needed for Google Calendar
+                    requestAccountsPermission();
+
+                    //check if user email is currently in the database, if not add it
+                    final DatabaseReference userReference = mUsersDatabaseReference.child(user.getUid());
+                    userReference.addValueEventListener(new ValueEventListener() {
+                        @Override
+                        public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                            if(!dataSnapshot.exists()){
+                                userReference.setValue(user.getEmail());
+                            }
+                        }
+
+                        @Override
+                        public void onCancelled(@NonNull DatabaseError databaseError) {
+                            Log.d("onCancelledUserValue: ", databaseError.getDetails());
+                        }
+                    });
                 }
                 else{
                     //user is signed out
@@ -97,5 +197,35 @@ public class MainActivity extends AppCompatActivity {
                 }
             }
         };
+    }
+
+    private void requestAccountsPermission(){
+        if (ContextCompat.checkSelfPermission(this, GET_ACCOUNTS)
+                != PackageManager.PERMISSION_GRANTED) {
+            //Permission is not granted
+            if (ActivityCompat.shouldShowRequestPermissionRationale(this,
+                    GET_ACCOUNTS)) {
+            } else {
+                //request the permission
+                ActivityCompat.requestPermissions(this,
+                        new String[]{GET_ACCOUNTS}, MY_PERMISSIONS_REQUEST_GET_ACCOUNTS);
+            }
+        } else {
+            Log.d("accountPermission: ", "permission has already been granted");
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+
+        switch (requestCode){
+            case MY_PERMISSIONS_REQUEST_GET_ACCOUNTS:
+                if(grantResults[0] == PackageManager.PERMISSION_GRANTED){
+                    Log.d("PermissionsResult: ", "view accounts granted");
+                }
+                else
+                    Toast.makeText(this, "denied", Toast.LENGTH_LONG).show();
+        }
     }
 }
